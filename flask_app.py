3e5ae6c28c612a209ffc6205ed10e6931c4a9810
Syzygy <zcemkap@ucl.ac.uk>
2024-10-25 14:08:31 +0100

    Added a service layer, and tests for service and API layers

---
+++ flask_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import services
+import repository
+
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.get_postgress_uri()))
+app = Flask(__name__)
+
+@app.route("/allocate", methods=['POST'])
+def allocate_endpoint():
+    session = get_session()
+    repo = repository.SqlRespository(session)
+    line = model.OrderLine(
+        request.json['orderid'],
+        request.json['sku'],
+        request.json['qty']
+    )
+    
+    try:
+        batchref = services.allocate(line, repo, session)
+    except:
+        (model.OutOfStock, services.InvalidSku) as e:
+            return jsonify({'message' : str(e)}), 400
+        
+    return jsonify({'batchref' : batchref}), 201
+    
+    
